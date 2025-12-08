033a5c0d25dfce993d73f9d2a93cabd3d4e0fc25
maryam13188 <roro13188@gmail.com>
2025-12-08 11:02:47 +0000

    style: Fix line length in binary_tree_postorder
    - Reduce line 4 from 81 to 74 characters
    - Change description to 'Post-order traversal of binary tree'
    - Ensure Betty compliance (max 80 chars per line)

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Goes through a binary tree using post-order traversal
+ * binary_tree_postorder - Post-order traversal of binary tree
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  *
@@ -13,7 +13,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	/* Post-order: Left -> Right -> Root */
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	func(tree->n);
