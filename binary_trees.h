48dfff259ddeb9d0225f7ffcfea90b3357fa3cf3
maryam13188 <roro13188@gmail.com>
2025-12-08 10:54:20 +0000

    feat: Implement binary_tree_postorder function (Task 8)
    - Perform post-order traversal (Left → Right → Root)
    - Recursive implementation following post-order definition
    - Handle NULL tree or function pointer gracefully
    - Process children before parent node

--- binary_trees.h
+++ binary_trees.h
@@ -1,33 +1,31 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stdlib.h>
 #include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
+ *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* Binary tree print function */
-void binary_tree_print(const binary_tree_t *);
+void binary_tree_print(const binary_tree_t *tree);
 
-/* Task functions */
+/* Task functions 0-7 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -35,5 +33,8 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 8: Post-order traversal */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
