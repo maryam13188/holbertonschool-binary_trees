58ec2d04fbf6645af50d565612da67f8da8abb94
maryam13188 <roro13188@gmail.com>
2025-12-08 10:33:56 +0000

    build: Add inorder traversal prototype to header

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,25 @@
+#include "binary_tables.h"
+
+/**
+ * binary_tree_inorder - Goes through a binary tree using in-order traversal
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+ *
+ * Return: Nothing
+ * Description: If tree or func is NULL, do nothing
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	/* Check if tree or func is NULL */
+	if (tree == NULL || func == NULL)
+		return;
+
+	/* Traverse left subtree */
+	binary_tree_inorder(tree->left, func);
+	
+	/* Visit the root node */
+	func(tree->n);
+	
+	/* Traverse right subtree */
+	binary_tree_inorder(tree->right, func);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -1,52 +1,39 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdlib.h>
 #include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-/* Binary Search Tree type */
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
-
-/* AVL Tree type */
 typedef struct binary_tree_s avl_t;
-
-/* Max Binary Heap type */
 typedef struct binary_tree_s heap_t;
 
-/* Function to print binary tree */
-void binary_tree_print(const binary_tree_t *tree);
+/* Binary tree print function */
+void binary_tree_print(const binary_tree_t *);
 
-/* Task 0: Create node */
+/* Task functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
-/* Task 1: Insert left */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-
-/* Task 2: Insert right */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-
-/* Task 3: Delete tree */
 void binary_tree_delete(binary_tree_t *tree);
-
-/* Task 4: Check if leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
-
-/* Task 6: Pre-order traversal */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
