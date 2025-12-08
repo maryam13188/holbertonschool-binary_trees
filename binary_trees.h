b59624b0d714f912294741c759ee2e6c8597e759
maryam13188 <roro13188@gmail.com>
2025-12-08 10:09:07 +0000

    build: Update header file for pre-order traversal
    - Add function prototype for binary_tree_preorder
    - Include <stddef.h> for NULL definition
    - Add descriptive comments to binary_tree_s structure
    - Define type aliases: bst_t, avl_t, heap_t
    - Prepare header for task 6 implementation

--- binary_trees.h
+++ binary_trees.h
@@ -11,29 +11,42 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+/* Binary Search Tree type */
 typedef struct binary_tree_s bst_t;
+
+/* AVL Tree type */
 typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap type */
 typedef struct binary_tree_s heap_t;
 
-/* Function prototypes for tasks 0-3 */
+/* Function to print binary tree */
+void binary_tree_print(const binary_tree_t *tree);
+
+/* Task 0: Create node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Task 1: Insert left */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* Task 2: Insert right */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* Task 3: Delete tree */
 void binary_tree_delete(binary_tree_t *tree);
 
-/* Function prototype for task 5 */
-int binary_tree_is_root(const binary_tree_t *node);
+/* Task 4: Check if leaf */
+int binary_tree_is_leaf(const binary_tree_t *node);
 
-/* Helper function (provided by project) */
-void binary_tree_print(const binary_tree_t *);
+/* Task 6: Pre-order traversal */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
