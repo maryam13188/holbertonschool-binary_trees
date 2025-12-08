6aac16aba36dff945a9b3b67148762bb647e60ae
Randa-hb10 <rm-400000@hotmail.com>
2025-12-08 09:59:52 +0000

    Task 5: Implement binary_tree_is_root function
    - Create function that checks if a node is a root
    - Return 1 if node is root (parent is NULL)
    - Return 0 if node is not root or if node is NULL
    - Simple O(1) time complexity check
    - Follow Betty coding style guidelines

--- binary_trees.h
+++ binary_trees.h
@@ -2,48 +2,38 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
+ *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
-/* Binary Search Tree */
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
-
-/* AVL Tree */
 typedef struct binary_tree_s avl_t;
-
-/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
-/* Task 0: New node */
+/* Function prototypes for tasks 0-3 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
-/* Task 1: Insert left */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-
-/* Task 2: Insert right */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-
-/* Task 3: Delete */
 void binary_tree_delete(binary_tree_t *tree);
 
-/* Task 4: Is leaf */
-int binary_tree_is_leaf(const binary_tree_t *node);
+/* Function prototype for task 5 */
+int binary_tree_is_root(const binary_tree_t *node);
 
-/* Provided helper function (for visualization only) */
-void binary_tree_print(const binary_tree_t *tree);
+/* Helper function (provided by project) */
+void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
