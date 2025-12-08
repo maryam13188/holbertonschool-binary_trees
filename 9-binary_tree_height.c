72ec13672f0ab833c08d77e36e61fa9b028b33f6
maryam13188 <roro13188@gmail.com>
2025-12-08 11:37:58 +0000

    fix: Final height calculation correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,22 +4,22 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure
  *
- * Return: Height of the tree, 0 if tree is NULL
+ * Return: Height of the tree, 0 if tree is NULL or single node
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	/* Calculate height of left subtree */
+	/* If leaf node, height is 0 */
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
 	left_height = binary_tree_height(tree->left);
-	
-	/* Calculate height of right subtree */
 	right_height = binary_tree_height(tree->right);
-	
-	/* Return the larger height + 1 (for current node) */
+
 	if (left_height > right_height)
 		return (left_height + 1);
 	else
