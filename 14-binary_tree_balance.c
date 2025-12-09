ea056bc4525032e1e1869727c7a9c367427c7676
Randa-hb10 <rm-400000@hotmail.com>
2025-12-09 06:36:44 +0000

    fix 14- binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,16 +8,39 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+    size_t left_height = 0;
+    size_t right_height = 0;
 
     if (tree == NULL)
         return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+    if (tree->left)
+        left_height = 1 + binary_tree_height(tree->left);
+    if (tree->right)
+        right_height = 1 + binary_tree_height(tree->right);
 
     if (left_height > right_height)
-        return (left_height + 1);
+        return (left_height);
     else
-        return (right_height + 1);
+        return (right_height);
+}
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: Balance factor, 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int left_height = 0;
+    int right_height = 0;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = (int)binary_tree_height(tree->left);
+    right_height = (int)binary_tree_height(tree->right);
+
+    return (left_height - right_height);
 }
