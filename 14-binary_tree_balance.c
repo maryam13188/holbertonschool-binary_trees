c541f5881e0547ae241dc017fc349ec5b117f68f
Randa-hb10 <rm-400000@hotmail.com>
2025-12-09 06:46:34 +0000

    Fix: Correct height calculation in binary_tree_balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,15 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t l = 0, r = 0;
+size_t l = 0, r = 0;
 
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    return (l > r ? l : r);
+return (l > r ? l : r);
 }
 
 /**
@@ -27,13 +27,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_h, right_h;
+int left_h, right_h;
 
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    left_h = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
-    right_h = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
+left_h = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
+right_h = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
 
-    return (left_h - right_h);
+return (left_h - right_h);
 }
