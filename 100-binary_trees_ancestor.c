a049c76377886aea0c4b587db9f7e1aad891be8b
maryam13188 <roro13188@gmail.com>
2025-12-11 10:14:25 +0000

    Task 19: Implement binary_trees_ancestor function

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,40 +1,86 @@
-#include "binary_trees.h"
+#include <stddef.h>
 
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- * @first: Pointer to the first node.
- * @second: Pointer to the second node.
+ * struct binary_tree_s - Binary tree node
  *
- * Return: Pointer to the lowest common ancestor node of the two given nodes.
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
  */
-binary_tree_t *binary_trees_ancestor(
-		const binary_tree_t *first, const binary_tree_t *second)
+struct binary_tree_s
 {
-	/* Temporary pointer to traverse the ancestors of the second node */
-	binary_tree_t *tmp = NULL;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-	/* If either node is NULL, return NULL */
-	if (!first || !second)
-		return (NULL);
+typedef struct binary_tree_s binary_tree_t;
 
-	/* Traverse the ancestors of the first node */
-	while (first)
-	{
-		/* Start from the second node */
-		tmp = (binary_tree_t *)second;
-		/* Traverse the ancestors of the second node */
-		while (tmp)
-		{
-			/* If the nodes are the same, return the common ancestor */
-			if (first == tmp)
-				return (tmp);
-			/* Move to the parent node */
-			tmp = tmp->parent;
-		}
-		/* Move to the parent node */
-		first = first->parent;
-	}
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
+ *
+ * Return: Pointer to the lowest common ancestor node, or NULL if not found
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+                                     const binary_tree_t *second)
+{
+    const binary_tree_t *temp1, *temp2;
+    int depth1 = 0, depth2 = 0;
 
-	/* If no common ancestor is found, return NULL */
-	return (NULL);
+    /* If either node is NULL, return NULL */
+    if (!first || !second)
+        return (NULL);
+
+    /* If both nodes are the same, return the node itself */
+    if (first == second)
+        return ((binary_tree_t *)first);
+
+    /* Calculate depth of first node */
+    temp1 = first;
+    while (temp1->parent)
+    {
+        depth1++;
+        temp1 = temp1->parent;
+    }
+
+    /* Calculate depth of second node */
+    temp2 = second;
+    while (temp2->parent)
+    {
+        depth2++;
+        temp2 = temp2->parent;
+    }
+
+    /* Reset pointers to original nodes */
+    temp1 = first;
+    temp2 = second;
+
+    /* Move the deeper node up until both are at same depth */
+    while (depth1 > depth2)
+    {
+        temp1 = temp1->parent;
+        depth1--;
+    }
+
+    while (depth2 > depth1)
+    {
+        temp2 = temp2->parent;
+        depth2--;
+    }
+
+    /* Move both nodes up together until they meet or reach NULL */
+    while (temp1 && temp2)
+    {
+        if (temp1 == temp2)
+            return ((binary_tree_t *)temp1);
+        temp1 = temp1->parent;
+        temp2 = temp2->parent;
+    }
+
+    /* If no common ancestor found, return NULL */
+    return (NULL);
 }
