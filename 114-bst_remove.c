2cabca1ace5917d3c24097e97ba040fc00290e43
Randa-hb10 <rm-400000@hotmail.com>
2025-12-14 08:03:18 +0000

    Fix Betty: split bst_remove into smaller functions

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -16,6 +16,35 @@ static bst_t *bst_find_min(bst_t *node)
 }
 
 /**
+ * bst_remove_one_child - Removes node with one or no child
+ * @root: Node to remove
+ * Return: New node to replace the removed one
+ */
+static bst_t *bst_remove_one_child(bst_t *root)
+{
+	bst_t *temp = NULL;
+
+	if (root->left == NULL)
+	{
+		temp = root->right;
+		if (temp != NULL)
+			temp->parent = root->parent;
+		free(root);
+		return (temp);
+	}
+	else if (root->right == NULL)
+	{
+		temp = root->left;
+		if (temp != NULL)
+			temp->parent = root->parent;
+		free(root);
+		return (temp);
+	}
+
+	return (root);
+}
+
+/**
  * bst_remove - Removes a node from a Binary Search Tree
  * @root: Pointer to the root node of the tree
  * @value: Value to remove in the tree
@@ -42,28 +71,19 @@ bst_t *bst_remove(bst_t *root, int value)
 	}
 	else
 	{
-		if (root->left == NULL)
+		if (root->left == NULL || root->right == NULL)
 		{
-			temp = root->right;
-			if (temp != NULL)
-				temp->parent = root->parent;
-			free(root);
-			return (temp);
+			return (bst_remove_one_child(root));
 		}
-		else if (root->right == NULL)
+		else
 		{
-			temp = root->left;
-			if (temp != NULL)
-				temp->parent = root->parent;
-			free(root);
-			return (temp);
+			/* Node with two children */
+			temp = bst_find_min(root->right);
+			root->n = temp->n;
+			root->right = bst_remove(root->right, temp->n);
+			if (root->right != NULL)
+				root->right->parent = root;
 		}
-
-		temp = bst_find_min(root->right);
-		root->n = temp->n;
-		root->right = bst_remove(root->right, temp->n);
-		if (root->right != NULL)
-			root->right->parent = root;
 	}
 
 	return (root);
